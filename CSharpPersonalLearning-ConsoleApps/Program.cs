2fb24774109e38bc6e8f99610f66350b49eec9a4
Jean-Marc Flamand <7jtf@videotron.ca>
2019-11-15 12:03:31 -0500

    Add 2 projects

---
+++ CSharpPersonalLearning-ConsoleApps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPersonalLearning_ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
