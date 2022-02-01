6f43035c27ee76330d6ac66ff5f45c993d155fdd
Jean-Marc Flamand <7jtf@videotron.ca>
2022-02-01 08:53:39 -0500

    Add projects Sum Single List With some Null Values

---
+++ SumListWithsomeNullValues/Program.cs
@@ -0,0 +1,28 @@
+﻿List<int?> numberList = new();
+numberList.Add(32);
+numberList.Add(21);
+numberList.Add(null);
+numberList.Add(11);
+numberList.Add(89);
+int? result = 0;
+
+foreach (var item in numberList)
+{
+    result += item.GetValueOrDefault();
+}
+
+Console.WriteLine($"without if statement Value is : {result}");
+
+//Reset result
+result = 0;
+
+foreach (var item in numberList)
+{
+    if (item != null)
+    {
+        result += item.GetValueOrDefault() ;
+    }
+    
+}
+
+Console.WriteLine($"with if statement Value is : {result}");
