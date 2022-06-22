62a7aa1e7ec29d553e0d329c4462db3a4ea72037
Jean-Marc Flamand <7jtf@videotron.ca>
2022-06-22 07:07:40 -0400

    Add Console Lunch Default Browser and NavigateTo URL project

---
+++ ConsoleLunchDefaultBrowserandNavigateToURL/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+try
+{
+    string request = "https://www.google.com";
+    System.Diagnostics.ProcessStartInfo ps = new System.Diagnostics.ProcessStartInfo
+    {
+        FileName = request,
+        UseShellExecute = true
+    };
+    System.Diagnostics.Process.Start(ps);
+}
+catch (Exception err)
+{
+    Console.WriteLine(err.Message);
+}
