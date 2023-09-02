6441aa666823ad1e3f10deb24ace4ad75d072ce6
Jean-Marc Flamand <7jtf@videotron.ca>
2023-09-02 08:54:11 -0400

    Update latest coding style.

--- GetSNfromSNrange/Program.cs
+++ GetSNfromSNrange/Program.cs
@@ -1,70 +1,66 @@
 ﻿
-class Program
+bool continueApp = true;
+
+while (continueApp)
 {
-    static bool getsnfromsnrange(int valueToFind, string range)
+    Console.Write("Entrez la valeur à rechercher : ");
+    int valueToFind = int.Parse(Console.ReadLine());
+
+    Console.Write("Entrez la plage de valeurs (utilisez le format '4-251, 255, 257-625, 627-660, 672-715') : ");
+    string range = Console.ReadLine();
+
+    bool found = getsnfromsnrange(valueToFind, range);
+
+    if (found)
     {
-        // Séparer les plages et valeurs individuelles en utilisant ","
-        string[] ranges = range.Split(',');
-
-        foreach (string rangeStr in ranges)
-        {
-            string[] parts = rangeStr.Split('-');
-
-            if (parts.Length == 1)
-            {
-                // Cas d'une valeur unique
-                int singleValue = int.Parse(parts[0].Trim());
-                if (valueToFind == singleValue)
-                {
-                    return true;
-                }
-            }
-            else if (parts.Length == 2)
-            {
-                // Cas d'une plage
-                int start = int.Parse(parts[0].Trim());
-                int end = int.Parse(parts[1].Trim());
-
-                if (valueToFind >= start && valueToFind <= end)
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        Console.WriteLine($"La valeur {valueToFind} est présente dans la plage.");
+    }
+    else
+    {
+        Console.WriteLine($"La valeur {valueToFind} n'est pas présente dans la plage.");
     }
 
-    static void Main()
+    Console.Write("Voulez-vous faire un autre exemple ? (O/N) : ");
+    string response = Console.ReadLine().Trim();
+
+    if (response.Equals("N", StringComparison.OrdinalIgnoreCase))
     {
-        bool continueApp = true;
+        continueApp = false;
+    }
+}
 
-        while (continueApp)
+
+
+static bool getsnfromsnrange(int valueToFind, string range)
+{
+    // Séparer les plages et valeurs individuelles en utilisant ","
+    string[] ranges = range.Split(',');
+
+    foreach (string rangeStr in ranges)
+    {
+        string[] parts = rangeStr.Split('-');
+
+        if (parts.Length == 1)
         {
-            Console.Write("Entrez la valeur à rechercher : ");
-            int valueToFind = int.Parse(Console.ReadLine());
-
-            Console.Write("Entrez la plage de valeurs (utilisez le format '4-251, 255, 257-625, 627-660, 672-715') : ");
-            string range = Console.ReadLine();
-
-            bool found = getsnfromsnrange(valueToFind, range);
-
-            if (found)
+            // Cas d'une valeur unique
+            int singleValue = int.Parse(parts[0].Trim());
+            if (valueToFind == singleValue)
             {
-                Console.WriteLine($"La valeur {valueToFind} est présente dans la plage.");
+                return true;
             }
-            else
-            {
-                Console.WriteLine($"La valeur {valueToFind} n'est pas présente dans la plage.");
-            }
+        }
+        else if (parts.Length == 2)
+        {
+            // Cas d'une plage
+            int start = int.Parse(parts[0].Trim());
+            int end = int.Parse(parts[1].Trim());
 
-            Console.Write("Voulez-vous faire un autre exemple ? (O/N) : ");
-            string response = Console.ReadLine().Trim();
-
-            if (response.Equals("N", StringComparison.OrdinalIgnoreCase))
+            if (valueToFind >= start && valueToFind <= end)
             {
-                continueApp = false;
+                return true;
             }
         }
     }
+
+    return false;
 }
