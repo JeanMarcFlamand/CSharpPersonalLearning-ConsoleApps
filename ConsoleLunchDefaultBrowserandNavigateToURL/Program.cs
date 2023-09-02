56923f10904f873d9590747d28fc14936384d0a2
Jean-Marc Flamand <7jtf@videotron.ca>
2023-09-02 07:11:34 -0400

    add new project

--- ConsoleLunchDefaultBrowserandNavigateToURL/Program.cs
+++ ConsoleLunchDefaultBrowserandNavigateToURL/Program.cs
@@ -1,17 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-try
-{
-    string request = "https://www.google.com";
-    System.Diagnostics.ProcessStartInfo ps = new System.Diagnostics.ProcessStartInfo
-    {
-        FileName = request,
-        UseShellExecute = true
-    };
-    System.Diagnostics.Process.Start(ps);
-}
-catch (Exception err)
-{
-    Console.WriteLine(err.Message);
-}
+//try
+//{
+//    string request = "https://www.google.com";
+//    System.Diagnostics.ProcessStartInfo ps = new System.Diagnostics.ProcessStartInfo
+//    {
+//        FileName = request,
+//        UseShellExecute = true
+//    };
+//    System.Diagnostics.Process.Start(ps);
+//}
+//catch (Exception err)
+//{
+//    Console.WriteLine(err.Message);
+//}
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://www.google.ca/");
+IWebElement element = driver.FindElement(By.Name("q"));
+element.SendKeys("Jean-Marc Flamand");
+
